50fabcbecf520b21b272d0f1d9d0b141c5680efe
Kirstin Kleyn <57179572+kmkleyn@users.noreply.github.com>
2020-06-09 11:55:10 +0200

    Update app.js
    A set of () is missing

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-kirstin:" + password.password + "@cluster0-x4ltc.mongodb.net/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-kirstin:" + password.password() + "@cluster0-x4ltc.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
